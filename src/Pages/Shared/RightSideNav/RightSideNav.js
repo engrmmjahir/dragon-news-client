3ed41faa9b49c62a8bb2de5c7a2144c61359d122
Jahir <engrmmjahir@gmail.com>
2022-10-18 17:24:48 +0600

    Implement Right Side Component andicons and added Navbar with responsive content

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram, FaWhatsapp, FaTwitch } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right Side Nav</h2>
+            <div>
+                <ButtonGroup vertical>
+                    <Button variant="outline-primary"><span className='text-warning'><FaGoogle></FaGoogle></span> Login with Google</Button>
+                    <Button variant="outline-dark mt-2"><FaGithub></FaGithub> Login with GitHub</Button>
+                </ButtonGroup>
+            </div>
+            <div className='mt-4'>
+                <h5>Find us on</h5>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaInstagram /> Instagram</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp /> Whatsapp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch /> Twitch</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <BrandCarousel></BrandCarousel>
+            </div>
         </div>
     );
 };
